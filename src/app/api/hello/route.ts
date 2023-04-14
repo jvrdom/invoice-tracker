29534b5563e24178673984f6c6bda8bf6059fc5a
Bruno Viera <brunovierag@gmail.com>
2023-04-14 08:48:24 -0300

    adding security to privated pages

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,9 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+
+export async function GET(request: Request, response: Response) {
+  const session = await getServerSession(authOptions);
+  console.log("bruno", session);
+
+  return new Response("Hello, Next.js!");
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard"], redirect: "/dashboard" };
+export const config = { matcher: ["/dashboard", "/api/hello"], redirect: "/" };
